7dd700e827e08f688f4be15a270b2e665d696527
Ankit Somani <ankit.m.somani@gmail.com>
2021-02-27 13:20:05 -0500

    saving index.js & generatedMarkdown.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const inquirer = require("inquirer");
+const path = require()
+
+
+const questions = [
+{
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+},
+{
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+},
+{
+        type: "list",
+        name: "licesne",
+        message: "What kind of licesne is your project using?",
+        choices: ["MIT", "APACHE", "NONE"],
+}
+]
+
+
+//function to write the readME file using the user input
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// write the function int intitialize the aplpication
+function init() {
+    inquirer.prompt(questions).then(inquirerResponses); => {
+        console.log("generating your readME");
+        writeToFile("READ.md", generateMarkdown({...inquirerResponses}))
+    }
+}
+
+init();
